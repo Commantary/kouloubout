518d6886785973c2146729c2fb56eeb6f8932e22
Commantary <babathiste@gmail.com>
2018-07-11 23:49:29 +0200

    Update command

--- module/trad.js
+++ module/trad.js
@@ -6,14 +6,16 @@ module.exports.run = async (client, message, args) => {
 		return message.channel.send("Il faut préciser un nom de pokémon !");
 	}
 
-	if (pokemon.getId(args[0], "fr") === -1){
+	var pokemonName = args[0].substr(0, 1).toUpperCase() + "" + args[0].substr(1).toLowerCase()
+
+	if (pokemon.getId(pokemonName, "fr") === -1){
 		return message.channel.send("Ce nom de pokémon n'existe pas !");
 	}
 
-		var pokemonId = pokemon.getId(args[0], "fr");
+		var pokemonId = pokemon.getId(pokemonName, "fr");
 
-		console.log("server: " + message.guild.name + " | channel: " + message.channel.name + " | author: " + message.author.username + " | getId: " + pokemon.getId(args[0], "fr") + " | getName: " + pokemon.getName(pokemonId))
+		console.log("server: " + message.guild.name + " | channel: " + message.channel.name + " | author: " + message.author.username + " | getId: " + pokemon.getId(pokemonName, "fr") + " | getName: " + pokemon.getName(pokemonId))
 
-		return message.channel.send("La traduction du pokémon **" + args[0] + "** est **" + pokemon.getName(pokemonId) + "**");
+		return message.channel.send("La traduction du pokémon **" + pokemonName + "** est **" + pokemon.getName(pokemonId) + "**");
 
 } // FIN DU MODULE EXPORTS
