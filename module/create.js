5aa9ca4add512cd8225a3f9b9db29a05deabe4e6
Commantary <babathiste@gmail.com>
2018-07-11 23:59:25 +0200

    Update messages

--- module/create.js
+++ module/create.js
@@ -10,53 +10,65 @@ got.get(bdd, {
 	json: true,
 }).then(data => {
 	var body = data.body;
-
-	body[message.author.id] = {
-				"username": message.author.username,
-				"bob_principale": "Aucun",
-				"bob": {
-					"0": {
-						"number": "0",
-						"name": "Pravou"
-					},
-					"1": {
-						"number": "0",
-						"name": "Zouzou"
-					},
-					"2": {
-						"number": "0",
-						"name": "Lanaine"
-					},
-					"3": {
-						"number": "0",
-						"name": "Zbeuboux"
-					},
-					"4": {
-						"number": "0",
-						"name": "Tirazux"
-					},
-					"5": {
-						"number": "0",
-						"name": "Bigassou"
-					},
-					"6": {
-						"number": "0",
-						"name": "Zooux"
-					},
-					"7": {
-						"number": "0",
-						"name": "Prank"
-					},
-					"8": {
-						"number": "0",
-						"name": "Appelle de Trump"
-					},
-					"9": {
-						"number": "0",
-						"name": "Clay U.S.B"
-					}
+	if (body[message.author.id] == undefined){
+		body[message.author.id] = {
+			"username": message.author.username,
+			"bob_principale": "Aucun",
+			"bob": {
+				"0": {
+					"number": "0",
+					"name": "Pravou"
+				},
+				"1": {
+					"number": "0",
+					"name": "Zouzou"
+				},
+				"2": {
+					"number": "0",
+					"name": "Lanaine"
+				},
+				"3": {
+					"number": "0",
+					"name": "Zbeuboux"
+				},
+				"4": {
+					"number": "0",
+					"name": "Tirazux"
+				},
+				"5": {
+					"number": "0",
+					"name": "Bigassou"
+				},
+				"6": {
+					"number": "0",
+					"name": "Zooux"
+				},
+				"7": {
+					"number": "0",
+					"name": "Prank"
+				},
+				"8": {
+					"number": "0",
+					"name": "Appelle de Trump"
+				},
+				"9": {
+					"number": "0",
+					"name": "Clay U.S.B"
 				}
 			}
+		}
+
+		return message.channel.send({embed: {
+			color: 15922601,
+			description: "Votre compte bober **" + message.author.username + "** a bien été créer"
+		}});
+
+	} else {
+		return message.channel.send({embed: {
+			color: 16406616,
+			description: "Vous avez déjà un compte bober"
+		}})
+	}
 	//Le put
 	got.put(bdd, {
 		json: true,
